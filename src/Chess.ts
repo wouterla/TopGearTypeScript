1cc19be8ca3dfd87bc8397f82a2e838601b11edb
Wouter Lagerweij <wouter.lagerweij@accenture.com>
2025-10-01 18:37:09 +0100

    After the Wednesday session

--- src/Chess.ts
+++ src/Chess.ts
@@ -2,6 +2,9 @@
 
 class Chess {
   board: string[][];
+  moves: string[];
+  isWhitesTurn: boolean;
+  error: string;
 
   constructor() {
     this.board = this.createBoard();
@@ -31,6 +34,8 @@ class Chess {
       ["P2", "P2", "P2", "P2", "P2", "P2", "P2", "P2"],
       ["R2", "N2", "B2", "Q2", "K2", "B2", "N2", "R2"],
     ];
+    this.isWhitesTurn = true;
+    this.moves = [];
 
     return this.board;
   }
@@ -39,12 +44,30 @@ class Chess {
     const fromArray = this.mapToBoard(from);
     const toArray = this.mapToBoard(to);
 
-    const piece = this.board[fromArray[0]][fromArray[1]];
+    const piece = this.getPieceAtPosition(fromArray[0], fromArray[1]);
+    this.error = this.errorFromInvalidMove(piece)
+    if(this.error !== ""){
+      return this.board
+    }
+    
+    this.setSquareAt(fromArray[0],fromArray[1], "");
+    this.setSquareAt(toArray[0],toArray[1], piece);
 
-    this.board[fromArray[0]][fromArray[1]] = "";
-    this.board[toArray[0]][toArray[1]] = piece;
-
+    this.moves.push(from + "-" + to);
+    this.isWhitesTurn = !this.isWhitesTurn;
     return this.board;
+  }
+
+  private errorFromInvalidMove(piece: string) {
+    if (piece === "") {
+      return "You must select a piece to move."
+    }
+
+    const pieceColor = piece.charAt(1);
+    if ((pieceColor === "2" && this.isWhitesTurn) || (pieceColor === "1" &&  !this.isWhitesTurn))  {
+      return "You cannot move twice";
+    } 
+    return ""
   }
 
   private mapToBoard(position: string): [number, number] {
@@ -55,6 +78,19 @@ class Chess {
 
     return [column, row];
   }
+
+  private getPieceAtPosition(row, column): string{
+
+    return this.board[row][column] 
+  }
+
+ private setSquareAt(row, column, input){
+    this.board[row][column] = input
+  }
+
+  public getMoves(): string[]{
+    return this.moves;
+  } 
 }
 
 export default Chess;
--- test/Chess.test.ts
+++ test/Chess.test.ts
@@ -65,4 +65,111 @@ describe("Chess test", () => {
 
     expect(newBoard).toStrictEqual<string[][]>(expectedBoard);
   });
+
+  //should have no moves at the start of the game
+  it("Should have no moves at the start of the game", () => {
+    const game = new Chess();
+    game.setUpGame();
+    expect(game.getMoves().length).toBe(0);
+    expect(game.isWhitesTurn).toBe(true);
+  });
+
+  // should show the last move
+  it("Should show the previous move", () => {
+    
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D2", "D3");
+    expect(game.getMoves().at(-1)).toBe("D2-D3");
+    game.move("D7", "D6");
+    expect(game.getMoves().at(-1)).toBe("D7-D6");
+  });
+
+  it('should not allow one player two consecutive moves', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D2", "D3");
+    game.move("D3", "D4");
+    expect(game.getMoves().at(-1)).toBe("D2-D3");
+    expect(game.getMoves().length).toBe(1);
+  });
+
+  it('should not allow a move from an empty square', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D3", "D4");
+    expect(game.getMoves().length).toBe(0)
+  });
+
+  it('should report error if player tries to move from an empty square', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("A5", "A4");
+    expect(game.error).toBe("You must select a piece to move.");
+  });
+
+  it('should report error if player tries to move twice', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D2", "D3");
+    game.move("D3", "D4");
+    expect(game.error).toBe("You cannot move twice");
+  });
+
+  it('should not report error if move was successful', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D2", "D3");
+    game.move("D3", "D4");
+    game.move("D7", "D6");
+    expect(game.error).toBe("");
+  });
+
+   
+
+
+
+
+  // A piece cannot move into a space occupied by its color
+  // A piece can only make a move according to its type
+  // A pawn can be promoted when it reaches a square at the edge of the other player's side
+
+  // TODO
+  // A piece can only make a move according to its type
+  // continue on tests
+});
+
+describe("Pawn movement", () => {
+  it('should allow a pawn to move one space forward', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D2", "D3");
+
+    expect(game.error).toBe("");
+    expect(game.getMoves().length).toBe(1);
+    expect(game.board[2][3]).toBe("P1");
+  });
+
+  it('should not be able to move backward', () => {
+    const game = new Chess();
+    game.setUpGame();
+
+    game.move("D2", "D3");
+    game.move("D7", "D6");
+    game.move("D3", "D2");
+    
+
+    expect(game.error).toBe("Illegal move for a pawn");
+    expect(game.getMoves().length).toBe(2);
+    expect(game.board[2][3]).toBe("P1");
+  });
+  
+
 });
